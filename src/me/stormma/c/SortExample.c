d634f03973a7ff55bccf0bb5536aae4b950864b6
StormMa <StormMaybin@gmail.com>
2017-10-16 11:57:25 +0800

    modify c code format.

--- src/me/stormma/c/SortExample.c
+++ src/me/stormma/c/SortExample.c
@@ -3,10 +3,11 @@
 
 static int aux[SIZE] = {0};
 
-void show_array(int * array) {
+void show_array(int* array) {
 	for (int i = 0; i < SIZE; i++) {
         printf("%d ", array[i]);
     }
+    printf("\n");
 }
 
 void exch(int* array, int a, int b) {
@@ -15,7 +16,7 @@ void exch(int* array, int a, int b) {
     array[a] = temp;
 }
 
-void select_sort(int * array) {
+void select_sort(int* array) {
 	for (int i = 0; i < SIZE - 1; i++) {
 		int min = i;
 		for (int j = i + 1; j < SIZE; j++) {
@@ -31,7 +32,7 @@ void select_sort(int * array) {
 	}
 }
 
-void bubble_sort(int * array) {
+void bubble_sort(int* array) {
 	for (int i = 0; i < SIZE - 1; i++) {
         for (int j = 0; j < SIZE - 1 - i; j++) {
             if (array[j] > array[j+1]) {
@@ -43,7 +44,7 @@ void bubble_sort(int * array) {
     }
 }
 
-void insert_sort(int * array) {
+void insert_sort(int* array) {
 	for (int i = 1; i < SIZE; i++) {
 		for (int j = i; j > 0; j--) {
 			if (array[j] < array[j-1]) {
@@ -55,8 +56,7 @@ void insert_sort(int * array) {
 	}
 }
 
-void shell_sort(int * array, int len) {
-	//init gap
+void shell_sort(int* array, int len) {
 	int gap = 1;
 	while (gap < len / 3) {
 		gap = gap * 3 + 1;
@@ -74,7 +74,7 @@ void shell_sort(int * array, int len) {
 	}
 }
 
-void _merge(int * array, int low, int mid, int high) {
+void _merge(int* array, int low, int mid, int high) {
 	int i = low;
 	int j = mid + 1;
 	for (int k = low; k <= high; k++) {
@@ -93,7 +93,7 @@ void _merge(int * array, int low, int mid, int high) {
 	}
 }
 
-void _merge_sort(int * array, int low, int high) {
+void _merge_sort(int* array, int low, int high) {
     if (high <= low)
         return;
     int mid = low + ((high - low) >> 1);
@@ -107,9 +107,8 @@ void merge_sort(int * array, int length) {
     _merge_sort(array, low, high);
 }
 
-int partition(int * array, int low, int high) {
+int partition(int* array, int low, int high) {
     int i = low, j = high + 1;
-    //切分元素
     int value = array[low];
     while (1) {
         while (array[++i] < value) {
@@ -122,7 +121,6 @@ int partition(int * array, int low, int high) {
         }
         if (i >= j)
             break;
-        //交换i, j
         exch(array, i, j);
     }
     exch(array, low, j);
@@ -138,15 +136,17 @@ void _quick_sort(int* array, int low, int high) {
     _quick_sort(array, j + 1, high);
 }
 
+void quick_sort(int* array, int length) {
+    _quick_sort(array, 0, length - 1);
+}
+
 void _quick_sort_3way(int* array, int low, int high) {
     if (high <= low) {
         return;
     }
     int lt = low, i = low + 1, gt = high;
-    //切分元素
     int value = array[low];
     while (i <= gt) {
-        // array[i]和value比较
         int cmp = array[i] - value;
         if (cmp < 0)
             exch(array, lt++, i++);
@@ -164,15 +164,34 @@ void quick_sort_3way(int * array, int length) {
 
 int main() {
 	int array[] = {3, 2, 1, 4, 9, 8};
-//	select_sort(array);
-//	bubble_sort(array);
-//	insert_sort(array);
-//	shell_sort(array, sizeof(array) / sizeof(array[0]));
-//	merge_sort(array, sizeof(array) / sizeof(array[0]));
-//	show_array(array);
-//    quick_sort(array, sizeof(array) / sizeof(array[0]));
-//    show_array(array);
-    quick_sort_3way(array, sizeof(array) / sizeof(array[0]));
+	select_sort(array);
+	printf("After select sort: \n");
+	show_array(array);
+
+	bubble_sort(array);
+	printf("After bubble sort: \n");
+	show_array(array);
+
+	insert_sort(array);
+	printf("After insert sort: \n");
+	show_array(array);
+
+	shell_sort(array, sizeof(array) / sizeof(array[0]));
+	printf("After shell sort: \n");
+	show_array(array);
+
+	merge_sort(array, sizeof(array) / sizeof(array[0]));
+	printf("After merge sort: \n");
+	show_array(array);
+
+
+    quick_sort(array, sizeof(array) / sizeof(array[0]));
+    printf("After quick sort: \n");
     show_array(array);
+
+    quick_sort_3way(array, sizeof(array) / sizeof(array[0]));
+    printf("After quick three-way sort: \n");
+    show_array(array);
+
     return 0;
 }
